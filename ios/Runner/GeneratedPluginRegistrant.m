33a8e0120fabaec062e7607126a6946ebb5a0910
sherlockbeard <b417044@iiit-bh.ac.in>
2019-04-07 13:27:17 +0530

    coverted to dart 2

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <launcher_assist/LauncherAssistPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [LauncherAssistPlugin registerWithRegistrar:[registry registrarForPlugin:@"LauncherAssistPlugin"]];
+}
+
+@end
